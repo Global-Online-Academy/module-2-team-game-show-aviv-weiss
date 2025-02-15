5740b43db88e9d377110e2e26f085ffd99d4dafc
Aviv Weiss <avivweiss@macbookpro.lan>
2025-02-15 13:59:00 +0100

    Added strategy 3 for which I was the driver

--- groupstrategies.py
+++ groupstrategies.py
@@ -1,13 +1,4 @@
-# YOU ONLY HAVE TO SUBMIT FUNCTIONS FOR WHICH
-# YOU ARE THE DRIVER IN PAIR PROGRAMMING
-
-
-# Here are some history variables to test your code on. Feel free to create your own.
-hist1 = []
-hist2 = [("split","steal")]
-hist3 = [("split","split"),("steal","split"),("split,steal"),("split,split"),("steal,split")]
-hist4 = [("split","steal"),("steal","steal"),("split","steal"),("steal","split"),("split","split"),("steal","split")]
-
+import random
 # Your team's 1st strategy (leave blank if you are not the driver)
 # Explanation of Strategy:
 # 
@@ -20,4 +11,33 @@ hist4 = [("split","steal"),("steal","steal"),("split","steal"),("steal","split")
 
 # Your team's 3rd strategy (leave blank if you are not the driver)
 # Explanation of Strategy:
-# 
+# Utilizes steal and split ratios to charecterize the opponent, then completes the best moves based on charecterization
+def goldenBallersStrategy3(history):
+
+    rounds_played = len(history)
+
+    if rounds_played <= 3: # peaceful start to exploit strategies that are based off of the first moves.
+        return "split"
+    
+    opponent_moves = [round[1] for round in history] # creates a list with all of the opponents moves
+    steal_count = opponent_moves.count("steal")
+    split_count = opponent_moves.count("split")
+
+    steal_ratio = steal_count/rounds_played # ratio of steals to number of rounds
+    split_ratio = split_count/rounds_played # ratio of splits to number of rounds
+
+
+    if steal_ratio > 0.6: # punishes "selfish" strategies
+        return "steal"
+      
+    elif split_ratio > 0.8: # exploites "naive" strategies
+        return "steal"
+        
+    elif steal_ratio < 0.3: # splits with unpredictable strategies
+        return "split" 
+        
+    elif steal_ratio == 0.5: #chooses randomly for strategies that do the same
+        return random.choice(["split", "steal"])
+
+    else: # steals 75% of the time as a last case scenerio
+        return random.choice(["steal", "steal", "steal", "split"])
